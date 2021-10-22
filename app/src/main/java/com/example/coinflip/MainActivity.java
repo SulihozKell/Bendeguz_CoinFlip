dd21cc8aac033943d195f4a3d413a1063dd28cf0
Nagy Bendegúz <bendeguz@medea.hu>
2021-10-22 23:47:07 +0200

    Az nem szép animációs részek kivéve.

--- app/src/main/java/com/example/coinflip/MainActivity.java
+++ app/src/main/java/com/example/coinflip/MainActivity.java
@@ -3,10 +3,7 @@ package com.example.coinflip;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.animation.ObjectAnimator;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.Handler;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,8 +19,6 @@ public class MainActivity extends AppCompatActivity {
     private int gyozelemDb, dobasokDb, veresegDb;
     private AlertDialog.Builder alertDialogVege;
     private String szoveg;
-    private ObjectAnimator flipAnimation1, flipAnimation2, flipAnimation3, flipAnimation4, flipAnimation5;
-    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +34,13 @@ public class MainActivity extends AppCompatActivity {
                 gyozelemDb++;
                 dobasokDb++;
                 gyozelem.setText("Győzelem: " + gyozelemDb);
-                dobasok.setText("Dobások: " + dobasokDb);
             }
             else {
                 veresegDb++;
                 dobasokDb++;
                 vereseg.setText("Vereség: " + veresegDb);
-                dobasok.setText("Dobások: " + dobasokDb);
             }
+            dobasok.setText("Dobások: " + dobasokDb);
             if (veresegDb >= 3 || gyozelemDb >= 3) {
                 vege();
             }
@@ -57,66 +51,35 @@ public class MainActivity extends AppCompatActivity {
                 gyozelemDb++;
                 dobasokDb++;
                 gyozelem.setText("Győzelem: " + gyozelemDb);
-                dobasok.setText("Dobások: " + dobasokDb);
             }
             else {
                 veresegDb++;
                 dobasokDb++;
                 vereseg.setText("Vereség: " + veresegDb);
-                dobasok.setText("Dobások: " + dobasokDb);
             }
+            dobasok.setText("Dobások: " + dobasokDb);
             if (veresegDb >= 3 || gyozelemDb >= 3) {
                 vege();
             }
         });
     }
 
-    private void forgas() {
-        handler.postDelayed(kepcsereFej,1000);
-        flipAnimation1.start();
-
-        flipAnimation2.setStartDelay(1000);
-        flipAnimation2.start();
-        handler.postDelayed(kepcsereIras,1000);
-
-        flipAnimation3.setStartDelay(2000);
-        flipAnimation3.start();
-        handler.postDelayed(kepcsereFej,2000);
-    }
-
     private int ermeKep() {
         int veletlenKep = random.nextInt(2);
-        forgas();
 
         if (veletlenKep == 0) {
+            erme.setImageResource(R.drawable.heads);
+
             Toast.makeText(getApplicationContext(), "Fej!", Toast.LENGTH_SHORT).show();
-            erme.setRotationY(0);
             return 0;
         }
         else {
-            flipAnimation4.setStartDelay(3000);
-            flipAnimation4.start();
-            handler.postDelayed(kepcsereIras,4000);
-            flipAnimation5.setStartDelay(4000);
-            flipAnimation5.start();
+            erme.setImageResource(R.drawable.tails);
+
             Toast.makeText(getApplicationContext(), "Írás!", Toast.LENGTH_SHORT).show();
             return 1;
         }
     }
-    
-    private Runnable kepcsereFej = new Runnable() {
-        @Override
-        public void run() {
-            erme.setImageResource(R.drawable.heads);
-        }
-    };
-
-    private Runnable kepcsereIras = new Runnable() {
-        @Override
-        public void run() {
-            erme.setImageResource(R.drawable.tails);
-        }
-    };
 
     private void ujJatek() {
         gyozelemDb = 0;
@@ -140,18 +103,8 @@ public class MainActivity extends AppCompatActivity {
 
         alertDialogVege.setCancelable(false).setTitle(szoveg)
                 .setMessage("Szeretne új játékot játszani?")
-                .setPositiveButton("Igen", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        ujJatek();
-                    }
-                })
-                .setNegativeButton("Nem", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-                });
+                .setPositiveButton("Igen", (dialog, which) -> ujJatek())
+                .setNegativeButton("Nem", (dialog, which) -> finish());
 
         AlertDialog dialog = alertDialogVege.create();
         dialog.show();
@@ -165,16 +118,5 @@ public class MainActivity extends AppCompatActivity {
         gyozelem = findViewById(R.id.gyozelem);
         vereseg = findViewById(R.id.vereseg);
         random = new Random();
-        flipAnimation1 = ObjectAnimator.ofFloat(erme, "rotationY", 0f, 90f);
-        flipAnimation2 = ObjectAnimator.ofFloat(erme, "rotationY", 90f, 180f);
-        flipAnimation3 = ObjectAnimator.ofFloat(erme, "rotationY", 270f, 360f);
-        flipAnimation4 = ObjectAnimator.ofFloat(erme, "rotationY", erme.getRotationY(), 90f);
-        flipAnimation5 = ObjectAnimator.ofFloat(erme, "rotationY", 90f, 180f);
-        flipAnimation1.setDuration(1000);
-        flipAnimation2.setDuration(1000);
-        flipAnimation3.setDuration(1000);
-        flipAnimation4.setDuration(1000);
-        flipAnimation5.setDuration(1000);
-        handler = new Handler();
     }
 }
